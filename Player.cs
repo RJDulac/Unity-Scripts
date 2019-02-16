0850a442c6c8efb7cf529e182eed11d2a2c9ce88
Ryan Dulac <analyticrye@gmail.com>
2019-02-16 07:52:23 -0500

    storing here to grab from another pc

---
+++ Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 startPosition;
+    void Start()
+    {
+
+
+        //Vector3 is a new position
+        //grab the current position = (assign) new position
+        //transform.position = new Vector3(0,0,0);
+
+        //assign through inspector with startPosition field
+        transform.position = startPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
